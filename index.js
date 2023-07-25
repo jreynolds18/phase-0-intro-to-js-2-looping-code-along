e7f0e9d5ae6b8374e5fbb5f63a83c611239f86c4
Jesse Reynolds <jessereynolds18@gmail.com>
2023-07-25 07:04:22 -0500

    adds an array of messages using a for loop, adds a countDown function that counts from 10 to 0 with a while loop

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+//const gifts = ["teddy bear", "drone", "doll"];
+
+//function wrapGifts(gifts) {
+    //for (let i = 0; i < gifts.length; i++) {
+        //console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    //}
+    // return gifts;
+//}
+
+//wrapGifts(gifts);
+
+const messageComplete = [];
+const names = ["Guadalupe", "Ollie", "Aki"];
+const cardEvent = "surprise";
+
+function writeCards(names, cardEvent) {
+  for (let x = 0; x < names.length; x++) {
+    messageComplete.push(`Thank you, ${names[x]}, for the wonderful ${cardEvent} gift!`);
+  }
+  return messageComplete;
+}
+
+console.log(messageComplete);
+
+function countDown() {
+    let number = 10
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
+countDown();
